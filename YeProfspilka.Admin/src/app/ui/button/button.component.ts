1ba2d835608ae629ef938642237276852f286074
Maks <lion20914king@gmail.com>
2023-04-27 13:30:51 +0300

    Final 3

--- YeProfspilka.Admin/src/app/ui/button/button.component.ts
+++ YeProfspilka.Admin/src/app/ui/button/button.component.ts
@@ -16,7 +16,7 @@ export class ButtonComponent implements OnInit {
 
 	styles(): string {
 		if (this.type === 'secondary')
-			return 'text-black w-fit duration-300 px-4 bg-transparent border-black border rounded-standart text-xl h-12 font-regular enabled:hover:bg-black/10'
+			return 'text-black w-fit duration-300 px-4 bg-transparent border-black border rounded-standart text-xl h-12 font-regular enabled:hover:bg-black/10 w-fit'
 
 		return 'w-full duration-300 disabled:opacity-30 h-12 font-regular rounded-standart text-xl bg-primary text-white enabled:hover:bg-primary/80 duration-200 disabled:cursor-not-allowed';
 	}
