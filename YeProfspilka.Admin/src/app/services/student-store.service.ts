7e413b4122ec850d305b55255f05b2a06978dcdb
Maksym Marianchuk <c-Maksym.Marianchuk@skykick.com>
2023-09-05 12:14:55 +0300

    Add auth token to Export users

--- YeProfspilka.Admin/src/app/services/student-store.service.ts
+++ YeProfspilka.Admin/src/app/services/student-store.service.ts
@@ -1,50 +1,55 @@
-import { ToastrService, } from 'ngx-toastr';
-import { catchError, EMPTY, map, Observable, switchMap, } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
 
-import { HttpClient, HttpResponse, } from '@angular/common/http';
-import { Inject, Injectable, } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { catchError, EMPTY, map, Observable, switchMap } from 'rxjs';
 
-import { ImportType, } from '../models/ImportType';
-import { UploadFileResults, } from '../models/UploadFileResults';
-import { DownloadService, } from './download.service';
-import { RestService, } from './rest.service';
+import { ImportType } from '../models/ImportType';
+import { UploadFileResults } from '../models/UploadFileResults';
+import { DownloadService } from './download.service';
+import { RestService } from './rest.service';
+import { TokenService } from './token.service';
 
 @Injectable({ providedIn: 'root' })
 export class StudentStoreService {
-	url = 'student-store';
+    url = 'student-store';
 
-	constructor(
-		private service: RestService<UploadFileResults>,
-		private http: HttpClient,
-		private downloadService: DownloadService,
-		private toastrService: ToastrService
-	) {}
+    constructor(
+        private service: RestService<UploadFileResults>,
+        private http: HttpClient,
+        private downloadService: DownloadService,
+        private toastrService: ToastrService,
+        private token: TokenService
+    ) {}
 
-	uploadUsers(file: File, importType: ImportType = ImportType.Replace) {
-		const formData = new FormData();
-		formData.append('file', file);
-		return this.service.postForm(`${this.url}/upload?importType=${importType}`, formData);
-	}
+    uploadUsers(file: File, importType: ImportType = ImportType.Replace) {
+        const formData = new FormData();
+        formData.append('file', file);
+        return this.service.postForm(`${this.url}/upload?importType=${importType}`, formData);
+    }
 
-	getAll(): Observable<any> {
-		return this.service.getAll(this.url);
-	}
+    getAll(): Observable<any> {
+        return this.service.getAll(this.url);
+    }
 
-	exportUsers(): Observable<void> {
-		return this.http
-			.get<HttpResponse<Blob>>(`${this.service.baseURL}/${this.url}/export`, {
-				observe: 'response',
-				responseType: 'blob' as 'json',
-			})
-			.pipe(
-				map((response) => {
-					this.downloadService.getDownload(response as unknown as HttpResponse<Blob>);
-					this.toastrService.success('Всіх користувачів було експортовано!');
-				}),
-				catchError(() => {
-					this.toastrService.error('Помилка при експорті користувачів!');
-					return EMPTY;
-				})
-			);
-	}
+    exportUsers(): Observable<void> {
+        return this.http
+            .get<HttpResponse<Blob>>(`${this.service.baseURL}/${this.url}/export`, {
+                observe: 'response',
+                responseType: 'blob' as 'json',
+                headers: {
+                    Authorization: `Bearer ${this.token.get()}`,
+                },
+            })
+            .pipe(
+                map((response) => {
+                    this.downloadService.getDownload(response as unknown as HttpResponse<Blob>);
+                    this.toastrService.success('Всіх користувачів було експортовано!');
+                }),
+                catchError(() => {
+                    this.toastrService.error('Помилка при експорті користувачів!');
+                    return EMPTY;
+                })
+            );
+    }
 }
