00d56b1b9a15a08c5484843477a5768e95423d58
Maks <lion20914king@gmail.com>
2024-10-05 21:10:33 +0300

    Align changes

--- src/admin-panel/src/environments/environment.ts
+++ src/admin-panel/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
     apiUrl: 'https://e-profspilka.com.ua/api',
     googleId: '58953637872-v5m2pl79vk9ela8l25ttc9rpm5tgtfpt.apps.googleusercontent.com',
     imgbbKey: '4b76823349508cfe6987b62ea7b72eb8',
-    clientUrl: 'https://www.e-profspilka.com.ua/',
+    clientUrl: 'https://e-profspilka.com.ua/',
 };
 
 /*
